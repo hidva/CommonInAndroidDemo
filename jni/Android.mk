2fe3f3048b0d94256ab7a488b98e3c6f26812228
pp__qq <p_qp__q@163.com>
2016-05-04 15:47:49 +0800

    feat: 使用 common 新 Android.mk 的 DEMO

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,30 +1,48 @@
 LOCAL_PATH := $(call my-dir)
 
+pp_qq_common_common_c_includes := $(LOCAL_PATH)/lib/common/src
+pp_qq_common_cipher_c_includes := $(LOCAL_PATH)/lib/common/src
+pp_qq_common_digest_c_includes := $(LOCAL_PATH)/lib/common/src
+pp_qq_common_exception_c_includes := $(LOCAL_PATH)/lib/common/src
+pp_qq_common_execute_time_get_c_includes := $(LOCAL_PATH)/lib/common/src
+pp_qq_common_formatter_c_includes := $(LOCAL_PATH)/lib/common/src
+pp_qq_common_glibc_cxx_wrap_c_includes := $(LOCAL_PATH)/lib/common/src
+pp_qq_common_hex_coding_c_includes := $(LOCAL_PATH)/lib/common/src
+pp_qq_common_log_c_includes := $(LOCAL_PATH)/lib/common/src
+pp_qq_common_rrid_c_includes := $(LOCAL_PATH)/lib/common/src
+pp_qq_common_string_c_includes := $(LOCAL_PATH)/lib/common/src
+
 include $(shell find $(LOCAL_PATH)/ -mindepth 2  -name 'Android.mk' -type f)
 
 include $(CLEAR_VARS)
 
+LOCAL_C_INCLUDES :=  $(LOCAL_PATH)/lib/common/src
+
 LOCAL_MODULE := common_test
 LOCAL_SRC_FILES := common_test.cc
 
+LOCAL_STATIC_LIBRARIES := pp_qq_common_common
+
 LOCAL_LDLIBS := -llog
 
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
+LOCAL_C_INCLUDES :=  $(LOCAL_PATH)/lib/common/src
 
 LOCAL_MODULE := formatter_test
 LOCAL_SRC_FILES := formatter_test.cc
-LOCAL_STATIC_LIBRARIES := pp_qq_common_string pp_qq_common_exception
+LOCAL_STATIC_LIBRARIES := pp_qq_common_string  pp_qq_common_formatter
 LOCAL_LDLIBS := -llog
 
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
+LOCAL_C_INCLUDES :=  $(LOCAL_PATH)/lib/common/src
 
 LOCAL_MODULE := hex_coding_test
 LOCAL_SRC_FILES := hex_coding_test.cc
-LOCAL_STATIC_LIBRARIES := pp_qq_common_hex_coding pp_qq_common_string pp_qq_common_exception
+LOCAL_STATIC_LIBRARIES := pp_qq_common_hex_coding pp_qq_common_string
 LOCAL_LDLIBS := -llog
 
 include $(BUILD_SHARED_LIBRARY)
@@ -34,7 +52,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := rrid_test
 LOCAL_SRC_FILES := rrid_test.cc
 LOCAL_LDLIBS := -llog
-LOCAL_CPP_FEATURES := rtti exceptions
+LOCAL_STATIC_LIBRARIES := pp_qq_common_rrid
+LOCAL_C_INCLUDES :=  $(LOCAL_PATH)/lib/common/src
 
 include $(BUILD_SHARED_LIBRARY)
 
--- jni/Application.mk
+++ jni/Application.mk
@@ -9,7 +9,6 @@ APP_ABI :=  armeabi-v7a
 
 APP_STL := gnustl_shared
 
-APP_CFLAGS := -I$(project_jni_path)/lib/common/src
 APP_CPPFLAGS := -std=gnu++11
 
 NDK_TOOLCHAIN_VERSION = 4.9
